20121b0d53b8fdb094624f1fdc1261f68afa9eb9
thermonuclearpenguin <danielpeng111@gmail.com>
2025-09-28 03:23:55 -0400

    hi

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,19 +3,21 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimatedSprite2D.animation = "roll"
+	$AnimatedSprite2D.play()
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 var frameIndex :int=0;
 func _process(delta: float) -> void:
-	var sprite = $AnimatedSprite2D;
-	if sprite.animation != "roll":
-		sprite.animation = "roll"
-		sprite.play()
+	var sprite = $AnimatedSprite2D
+	#if sprite.animation != "roll":
+		#sprite.animation = "roll"
+		#sprite.play()
 	
 	
-	if sprite.frame == sprite.frames.get_frame_count("roll") - 1:
-		sprite.stop()  # stops at last frame
+	#if sprite.frame == sprite.sprite_frames.get_frame_count("roll") - 1:
+	#	sprite.stop() 
 	
 	
 
