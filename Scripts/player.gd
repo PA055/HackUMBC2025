4411dd4bbc6993e4e216be31b6a9be709e6a14db
Praful Adiga <praful.adiga@gmail.com>
2025-09-28 03:57:34 -0400

    better colors and also player animation maybe

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,17 +4,23 @@ extends RigidBody2D
 @export var boostCooldown: float = 0.75
 var boostCooldownLeft = 0;
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("transition");
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	boostCooldownLeft -= delta;
-	var sprite = $AnimatedSprite2D;
-	if sprite.animation != "roll":
-		sprite.animation = "roll"
-		sprite.play()
+	if not $AnimatedSprite2D.is_playing():
+		$AnimatedSprite2D.rotate(8 * delta)
 
 func _on_momentum_collider_body_entered(body: Node2D) -> void:
-	if boostCooldownLeft <= 0:
+	if boostCooldownLeft <= 0 and body is CharacterBody2D:
 		var force: Vector2 = body.next_velocity() * velocityMultiplier;
 		if force.length_squared() > 0:
 			boostCooldownLeft = boostCooldown;
 		apply_central_force(force);
+
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	if $AnimatedSprite2D.animation == "transition" && $AnimatedSprite2D.frame == 5:
+		$AnimatedSprite2D.stop();
